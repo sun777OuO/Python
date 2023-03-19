df696e32e974c68e4cc6d01404307ee3c738e22c
sun777OuO <w7084503@gmail.com>
2023-03-19 11:42:09 +0800

    hahahahaha

---
+++ class1/homework.py
@@ -0,0 +1,27 @@
+'''
+請將下列變數做出正確的型態分類
+apple = 1000
+age = 12
+minor = False
+ = "David"
+weight = 64.5
+zip = "545"
+
+int(整數)  :apple,age
+bool(布林) :minor
+str(字串)  :name,zip
+float(浮點數、小數):weight
+
+example:
+ball = 100
+talk = 'hello'
+則要在型態列表中填入該變數
+int:ball
+str:talk
+
+記得將程式上傳到Git提交作業
+指令提示：
+git add .
+git commit -m "提示文字"
+git push
+'''

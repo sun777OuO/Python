bee715d6f548bd74d9e3c62cb3be99f3bb267bba
sun777OuO <w7084503@gmail.com>
2023-04-09 12:26:25 +0800

    OK

---
+++ class3/20230409.py
@@ -0,0 +1,51 @@
+# try:
+#     num = int(input("請輸入一個數字"))
+#     total = num + 1
+#     print(total)
+# except ValueError:
+#     print("請輸入數字")
+# except:                #程式錯執行錯誤時
+#     print("發生錯誤")
+# else:                  #程式執行成功時
+#     print("成功執行")
+# finally:
+#     print("程式結束")
+# 比較運算子   運算      範例      結果(布林)
+#  ==        相等      1 == 1       True
+# !=        不相等     1 != 0       True
+# >=        大於等於   1 >= 2       False
+# <=        小於等於   1 <= 2       True
+# >          大於      1 > 2       False
+# <          小於      1 < 2       True
+# not範例     結果(布林)
+# not False  True
+# not True   False
+# and範例         結果(布林)
+# False and  False  False
+# False and  True   False
+# True  and  False  False
+# True  and  True   True
+# or範例         結果(布林)
+# False or  False  False
+# False or  True   True
+# True  or  False  True
+# True  or  True   True
+# password = input("請輸入密碼:")
+# if password == "123456789":
+#     print("歡迎光臨...")
+# elif password == "987654321":
+#     print("歡迎.....")
+# else:
+#     print("哈哈哈哈哈哈哈哈哈哈哈,你玩不起,笑死你")
+
+# grade = int(input("請輸入成績:"))
+# if grade >= 90:
+#     print("A")
+# elif grade >= 80 and grade <= 89:
+#     print("B")
+# elif grade >= 70 and grade <= 79:
+#     print("C")
+# elif grade >= 60 and grade <= 69:
+#     print("D")
+# else:
+#     print("E")

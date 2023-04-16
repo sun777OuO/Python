a873474360c98209e44425643e67b083bc42489b
sun777OuO <w7084503@gmail.com>
2023-04-16 11:45:17 +0800

    123456789

---
+++ class4/20230416.py
@@ -0,0 +1,57 @@
+# math = int(input("請輸入數字:"))
+# if math % 2 != 1:
+#     print("偶數")
+# elif math % 2 == 1:
+#     print("奇數")
+# import turtle
+
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# # turtle.backward(100)
+# # turtle.right(9)
+# # turtle.left(100)
+# turtle.done()
+# for we in range(5):
+#     print(we)
+
+# for we in range(2, 6):
+#     print(we)
+
+# for we in range(2, 10, 2)
+#     print(we)
+# import turtle
+
+# for we in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.done()
+
+# import turtle
+
+# turtle.color("green")  # 設定小烏龜顏色
+# turtle.shape("turtle")
+# 設定小烏龜形'arrow','turtle','clircle', 'square','triangle', 'classic'.
+# turtle.stamp()  # 蓋章
+
+# turtle.penup()  # 提筆
+# for we in range(2, 100, 2):
+#     turtle.forward(we)
+#     turtle.right(30)
+#     turtle.stamp()
+# turtle.done()
+
+import turtle
+
+turtle.range(5)  # 線徑寬度1~10
+turtle.pencolor("green")  # 線的顏色
+turtle.forward(100)
+turtle.right(36)
+turtle.fillcolor("green")  # 區域填滿顏色
+turtle.begin_fill()  # 填滿區域設定開始
+turtle.end_fill()  # 填滿區域設定結束
---
+++ class4/homeworfk.py
@@ -0,0 +1,36 @@
+"""
+請使用turtle模組以及for印出以下圖形
+t0_turtle_stamp.jpg
+提示：
+turtle.home()是讓烏龜回到原點的指令
+"""
+# import turtle
+
+
+# turtle.penup()  # 提筆
+# for we in range(8):
+#     turtle.forward(100)
+#     turtle.stamp()  # 蓋章
+#     turtle.backward(100)
+
+#     turtle.right(45)
+# turtle.done()
+"""
+請使用turtle模組、for、time模組來印出時鐘的秒針
+基本功能參考影片t1_turtle_time.mp4
+提示：
+turtle.clear() # 清空畫面指令
+
+import time  # 匯入time模組
+time.sleep(1) # 延遲1秒
+"""
+# import time  # 匯入time模組
+
+# turtle.speed(0)
+# for we in range(60):
+#     turtle.forward(100)
+#     turtle.backward(100)
+#     time.sleep(1)  # 延遲1秒
+#     turtle.right(6)
+#     turtle.clear()
+# turtle.done()

d737b7d16849cb04e8b12d50c1dc4f16eb9a9de4
sun777OuO <w7084503@gmail.com>
2023-04-30 11:32:03 +0800

    !@#$%^&*()

---
+++ class6/20230430.py
@@ -0,0 +1,60 @@
+# for weeeeeeeeeee in range(2, 6):
+#     print(weeeeeeeeeee)
+# else:
+#     print("迴圈正常結束")
+
+# we = 2
+# while we < 6
+#     print(we)
+#     we += 1
+# else:
+#     print("迴圈正常結束")
+
+# we = 1
+# while we < 6:
+#     if we == 3:
+#         break
+#     print(we)
+#     we += 1
+
+#     for we in range(1, 6):
+#         if we == 3:
+#             break
+#         print(we)
+
+# break只能強制結束1層迴圈
+# we = 1
+# while we < 6:
+#     if we == :
+#         we += 1
+#         continue
+#     print(we)
+#     we += 1
+# while True:
+#     wee = int(input("請輸入編號:"))
+#     if wee == 1:
+#         print("蘋果汁")
+#     if wee == 2:
+#         print("柳橙汁")
+#     if wee == 3:
+#         print("葡萄汁")
+#     if wee == 4:
+#         print("關閉離開")
+#         break
+# import random
+
+# we = random.randrange(3)
+# random.randrange(0, 10, 2)
+# random.randint(1, 3)
+# random.randint(1, 10)
+# print(we)
+# awa = random.randrange(1, 101)
+# while True:
+#     wee = int(input("請輸入編號:"))
+#     if wee > awa:
+#         print("再小一點")
+#     if wee < awa:
+#         print("再大一點")
+#     if wee == awa:
+#         print("猜中了!!!!!!")
+#         break

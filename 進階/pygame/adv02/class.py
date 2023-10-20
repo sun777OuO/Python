d21bd4d2a4e1d190b0a1ad710edc9b09893a88b4
sun777OuO <w7084503@gmail.com>
2023-10-20 20:35:41 +0800

    adv02-2

--- 進階/pygame/adv02/class.py
+++ 進階/pygame/adv02/class.py
@@ -1,8 +1,18 @@
 ######################匯入模組######################
-from tkinter.tix import Tree
-from turtle import Screen
 import pygame
 import sys
+
+
+######################定義函式######################
+def check_click(pos, x_min, y_min, x_max, y_max):
+    """判斷滑鼠是否點擊在指定的區域內"""
+    x_match = x_min < pos[0] < x_max
+    y_match = y_min < pos[1] < y_max
+    if x_match and y_match:
+        return True
+    else:
+        return False
+
 
 ######################初始化######################
 pygame.init()  # 啟動Pygame
@@ -36,16 +46,25 @@ bg.fill((255, 255, 255))  # 畫布為白色(R, G, B)
 # 畫圓弧, (畫布, 顏色, [x, y, 寬, 高], 起始角度, 結束角度, 線寬)
 # pygame.draw.arc(bg, (255, 10, 0), [100, 100, 100, 50], math.radians(180), math.radians(0), 2)
 
+######################設定文字######################
+typeface = pygame.font.get_default_font()  # 取得系統字體
+font = pygame.font.Font(typeface, 24)  # 設定字體和大小
+title = font.render("Start", True, (0, 0, 0))  # 設定文字參數: 文字內容, 是否開啟反鋸齒, 文字顏色, 背景顏色
+tit_w = title.get_width()  # 取得文字寬度
+tit_h = title.get_height()  # 取得文字高度
+
 ######################循環偵測######################
 paint = False
 while True:
+    mouse_pos = pygame.mouse.get_pos()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # 如果按下  [X] 就退出
             sys.exit()  # 離開遊戲
 
         if event.type == pygame.MOUSEBUTTONDOWN:  # 按下滑鼠按鍵時
-            paint = not paint  # 狀態相反
-            print(paint)
+            if check_click(mouse_pos, 0, 0, tit_w, tit_h):  # 當滑鼠點擊特定區域
+                paint = not paint  # 狀態相反
 
     if paint:
         # 畫圓形, (畫布, 顏色, 圓心, 半徑, 線寬)
@@ -65,4 +84,5 @@ while True:
         bg.fill((255, 255, 255))  # 畫布為白色(R, G, B)
 
     screen.blit(bg, (0, 0))  # 繪製畫布於視窗左上角
+    screen.blit(title, (0, 0))  # 將文字畫在視窗的(0, 0)
     pygame.display.update()

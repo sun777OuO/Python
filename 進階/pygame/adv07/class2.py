880b17cba9b8a562bf0bcf87e87021104280c610
sun777OuO <w7084503@gmail.com>
2023-11-18 13:44:16 +0800

    adv07

--- 進階/pygame/adv07/class2.py
+++ 進階/pygame/adv07/class2.py
@@ -1,9 +1,9 @@
 ######################匯入模組######################
-from turtle import Turtle
 import pygame
 import sys
 import os
 from pygame.locals import *
+import random
 
 
 ####################定義函式######################
@@ -18,7 +18,11 @@ def bg_update():
 
 def move_dinosaur():
     """移動恐龍"""
-    global ds_y, jumpState, jumpValue, ds_index, ds_center_x, ds_center_y
+    global ds_y, jumpState, jumpValue, ds_index, ds_center_x, ds_center_y, ds_detect_r
+    if bend_down:
+        jumpState = False
+        ds_y = LIMIT_LOW + 20
+
     if jumpState:
         if ds_y >= LIMIT_LOW:
             jumpValue = -jump_height
@@ -32,15 +36,17 @@ def move_dinosaur():
             jumpState = False
             ds_y = LIMIT_LOW
 
-    ds_index = (ds_index - 1) % len(img_dinosaur)
-    ds_center_x = ds_x + img_dinosaur[ds_index].get_width() / 2
-    ds_center_y = ds_y + img_dinosaur[ds_index].get_height() / 2
-    screen.blit(img_dinosaur[ds_index], (ds_x, ds_y))
+    ds_index = (ds_index - 1) % len(ds_show)
+    ds_center_x = ds_x + ds_show[ds_index].get_width() / 2
+    ds_center_y = ds_y + ds_show[ds_index].get_height() / 2
+    ds_detect_r = min(ds_show[ds_index].get_width(), ds_show[ds_index].get_height()) / 2
+
+    screen.blit(ds_show[ds_index], (ds_x, ds_y))
 
 
 def move_cacti():
     """移動仙人掌"""
-    global cacti_x, score, cacti_y, cacti_x, cacti_center_x, cacti_center_y
+    global cacti_x, score, cacti_y, cacti_x, cacti_center_x, cacti_center_y, enemy_random
 
     cacti_x = (cacti_x - cacti_shift) % (bg_x - 100)
     cacti_center_x = cacti_x + img_cacti.get_width() / 2
@@ -49,6 +55,7 @@ def move_cacti():
 
     if cacti_x <= 0:
         score += 1
+        enemy_random = random.randint(0, 1)
 
 
 def score_update():
@@ -70,21 +77,46 @@ def game_over():
     screen.blit(img_gg, ((bg_x - gg_w) / 2, (bg_y - gg_h) / 2))
 
 
+def move_ptera():
+    """移動翼龍"""
+    global ptera_x, ptera_index, score, ptera_center_x, ptera_center_y, enemy_random
+    ptera_x = (ptera_x - ptera_shift) % (bg_x - 100)  # 翼龍移動
+    ptera_index = (ptera_index - 1) % len(img_ptera)
+    ptera_center_x = ptera_x + img_ptera[ptera_index].get_width() / 2
+    ptera_center_y = ptera_y + img_ptera[ptera_index].get_height() / 2
+    screen.blit(img_ptera[ptera_index], (ptera_x, ptera_y))
+    if ptera_x <= 0:
+        score += 1
+        enemy_random = random.randint(0, 1)
+
+
 ####################初始化######################
 os.chdir(sys.path[0])
 pygame.init()
 LIMIT_LOW = 140
+PTERA_LIMIT_LOW = 110
 clock = pygame.time.Clock()
 RED = (255, 0, 0)
+enemy_random = 0
 
 ####################載入圖片物件######################
 img = pygame.image.load("bg.png")
 img_dinosaur = [pygame.image.load("小恐龍1.png"), pygame.image.load("小恐龍2.png")]
 img_cacti = pygame.image.load("cacti.png")
 img_gg = pygame.image.load("gameover.png")
+img_ptera = [pygame.image.load("翼龍飛飛1.png"), pygame.image.load("翼龍飛飛2.png")]
+img_bend_down = [pygame.image.load("小恐龍蹲下1.png"), pygame.image.load("小恐龍蹲下2.png")]
 bg_x = img.get_width()
 bg_y = img.get_height()
 bg_roll_x = 0
+######################翼龍物件######################
+ptera_x = bg_x - 100
+ptera_y = PTERA_LIMIT_LOW
+ptera_index = 0
+ptera_shift = 10
+ptera_center_x = ptera_x + img_ptera[0].get_width() / 2
+ptera_center_y = ptera_y + img_ptera[0].get_height() / 2
+ptera_detect_r = max(img_ptera[0].get_width(), img_ptera[0].get_height()) / 2 - 10
 ######################建立視窗######################
 screen = pygame.display.set_mode([bg_x, bg_y])
 pygame.display.set_caption("Dinosaur")
@@ -102,6 +134,8 @@ jump_height = 13
 ds_center_x = ds_x + img_dinosaur[0].get_width() / 2
 ds_center_y = ds_y + img_dinosaur[0].get_height() / 2
 ds_detect_r = min(img_dinosaur[0].get_width(), img_dinosaur[0].get_height()) / 2
+ds_show = img_dinosaur
+bend_down = False  # 蹲下狀態
 
 ######################仙人掌物件######################
 cacti_x = bg_x - 100
@@ -117,7 +151,7 @@ gg_w = img_gg.get_width()
 gg_h = img_gg.get_height()
 ######################循環偵測######################
 while True:
-    clock.tick(100)
+    clock.tick(20)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -125,27 +159,61 @@ while True:
         if event.type == KEYDOWN:
             if event.key == K_SPACE and ds_y <= LIMIT_LOW:
                 jumpState = True
+            elif event.key == K_DOWN:
+                bend_down = True  # 開啟蹲下狀態
+                ds_show = img_bend_down  # 切換蹲下圖片
+
+            if event.key == K_RETURN and gg:
+                score = 0
+                gg = False
+                cacti_x = bg_x - 100
+                ptera_x = bg_x - 100
+                ds_y = LIMIT_LOW
+                jumpState = False
+        if event.type == KEYUP:
+            if event.key == K_DOWN:
+                bend_down = False  # 開啟蹲下狀態
+                ds_show = img_dinosaur  # 切換蹲下圖片
+                ds_y = LIMIT_LOW
+
     if gg:
         game_over()
     else:
         bg_update()
         move_dinosaur()
-        move_cacti()
         score_update()
 
-        gg = is_hit(
-            ds_center_x,
-            ds_center_y,
-            cacti_center_x,
-            cacti_center_y,
-            cacti_detect_r + ds_detect_r,
-        )
+        if enemy_random == 0:
+            move_cacti()
+            gg = is_hit(
+                ds_center_x,
+                ds_center_y,
+                cacti_center_x,
+                cacti_center_y,
+                cacti_detect_r + ds_detect_r,
+            )
+            pygame.draw.circle(
+                screen,
+                RED,
+                (int(cacti_center_x), int(cacti_center_y)),
+                cacti_detect_r + ds_detect_r,
+                1,
+            )
+        else:
+            move_ptera()
+            gg = is_hit(
+                ds_center_x,
+                ds_center_y,
+                ptera_center_x,
+                ptera_center_y,
+                ptera_detect_r + ds_detect_r,
+            )
+            pygame.draw.circle(
+                screen,
+                RED,
+                (int(ptera_center_x), int(ptera_center_y)),
+                ptera_detect_r + ds_detect_r,
+                1,
+            )
 
-        pygame.draw.circle(
-            screen,
-            RED,
-            (int(cacti_center_x), int(cacti_center_y)),
-            cacti_detect_r + ds_detect_r,
-            1,
-        )
     pygame.display.update()
